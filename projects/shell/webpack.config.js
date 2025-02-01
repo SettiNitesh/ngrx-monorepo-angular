0b3d409946dd7ceb7f27eac5a20656c3db35d36a
Nitesh <niteshs@byteridge.com>
2025-02-02 00:51:52 +0530

    Updated

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -3,27 +3,33 @@ const {
   withModuleFederationPlugin,
 } = require("@angular-architects/module-federation/webpack");
 
-import { environment } from "./src/environments/environment";
+// Create a wrapper function to accept environment
+module.exports = (env) => {
+  // Import environment file based on mode
+  const environment = require(`./src/environments/environment${
+    env.production ? ".prod" : ""
+  }.ts`).environment;
 
-module.exports = withModuleFederationPlugin({
-  name: "shell",
+  return withModuleFederationPlugin({
+    name: "shell",
 
-  exposes: {
-    "./Component": "./projects/shell/src/app/app.component.ts",
-  },
+    exposes: {
+      "./Component": "./projects/shell/src/app/app.component.ts",
+    },
 
-  remotes: {
-    mfe: `${environment.urls.mfe}/remoteEntry.js`,
-    "akita-counter": `${environment.urls.akitaCounter}/remoteEntry.js`,
-    flights: `${environment.urls.flights}/remoteEntry.js`,
-    hospital: `${environment.urls.hospital}/remoteEntry.js`,
-  },
+    remotes: {
+      mfe: `${environment.urls.mfe}/remoteEntry.js`,
+      "akita-counter": `${environment.urls.akitaCounter}/remoteEntry.js`,
+      flights: `${environment.urls.flights}/remoteEntry.js`,
+      hospital: `${environment.urls.hospital}/remoteEntry.js`,
+    },
 
-  shared: {
-    ...shareAll({
-      singleton: true,
-      strictVersion: true,
-      requiredVersion: "auto",
-    }),
-  },
-});
+    shared: {
+      ...shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: "auto",
+      }),
+    },
+  });
+};
