273ca5b665d43372cd320c6c9b55110e41792a7c
Nitesh <niteshs@byteridge.com>
2025-02-04 09:57:42 +0530

    Updated code

--- projects/shell/src/environments/environment.prod.ts
+++ projects/shell/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const PROD_ENV_URL =
 export const environment = {
   production: true,
   urls: {
-    mfe: `${PROD_ENV_URL}/mfe`,
+    mfe: PROD_ENV_URL,
     akitaCounter: PROD_ENV_URL,
     flights: PROD_ENV_URL,
     hospital: PROD_ENV_URL,
