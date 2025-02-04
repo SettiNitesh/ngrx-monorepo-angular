4929dce85f276376a78ded297043e77a2bade418
Nitesh <niteshs@byteridge.com>
2025-02-04 14:57:20 +0530

    Updated prod file

--- projects/shell/src/environments/environment.prod.ts
+++ projects/shell/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   urls: {
-    mfe: 'https://ngrx-monorepo-angular-4at6.vercel.app',
-    akitaCounter: 'https://ngrx-monorepo-angular.vercel.app',
-    flights: 'https://ngrx-monorepo-angular-egov.vercel.app',
-    hospital: 'https://ngrx-monorepo-angular-qkqn.vercel.app',
+    mfe: 'https://settinitesh.github.io/ngrx-monorepo-angular',
+    akitaCounter: 'https://settinitesh.github.io/ngrx-monorepo-angular',
+    flights: 'https://stunning-bavarois-9e6e52.netlify.app/',
+    hospital: 'https://settinitesh.github.io/ngrx-monorepo-angular',
   },
 };
